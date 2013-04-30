e48bdc5a1b0210ce7b70132670dd9842a3ba0ec3
Humberto <hlsp999@gmail.com>
2013-04-30 18:56:36 +0100

    landing page

--- app/models/livro.rb
+++ app/models/livro.rb
@@ -1,3 +1,7 @@
 class Livro < ActiveRecord::Base
   attr_accessible :ano_escolaridade, :autor_id, :editora_id, :nome, :preco
+
+  def self.simple_search(search_text)
+    search_text ? where("nome LIKE ?", search_text) : where(1)
+  end
 end
