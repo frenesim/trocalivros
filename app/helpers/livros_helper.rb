fa87f27c0425dd974a2ff68ec17ab2bad4227213
Humberto <hlsp999@gmail.com>
2013-05-29 16:35:28 +0100

    users can only edit their books

--- app/helpers/livros_helper.rb
+++ app/helpers/livros_helper.rb
@@ -1,2 +1,5 @@
 module LivrosHelper
+  def owner_link(livro)
+    link_to t('helpers.links.edit', :default => t("helpers.links.details")), edit_livro_path(livro), :class => 'btn btn-link' if current_user.id == livro.user_id
+  end
 end
