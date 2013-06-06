a66e68205bed1d289d8530c003ff958f38c3a41c
Humberto <hlsp999@gmail.com>
2013-06-06 18:37:32 +0100

    preparing for cucumber

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,8 +3,7 @@ require 'faker'
 
 feature "register user" do
 
-  scenario 'user registers without name and it appears a personalized welcome message with the email' do
-    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+  scenario 'user registers without name and it appears a personalized welcome message with his email' do
     visit new_user_registration_path
     email = Faker::Internet.email
     fill_in :user_email, with: email
@@ -12,16 +11,18 @@ feature "register user" do
     fill_in :user_password_confirmation, with: "password"
     click_on "Registo"
     page.should have_content(email)
-    #get autores_path
-    #response.status.should be(302)
   end
 
-  scenario 'user registers with name and it appears a personalized welcome message with the name' do
-
-  end
-
-  scenario "unauthenticated user can see list of books" do
-    visit livros_path
+  scenario 'user registers with name and it appears a personalized welcome message with his name' do
+    visit new_user_registration_path
+    name = Faker::Name.name
+    email = Faker::Internet.email
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: "password"
+    fill_in :user_password_confirmation, with: "password"
+    click_on "Registo"
+    page.should have_content(name)
   end
 
 end
