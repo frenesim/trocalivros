852a2a3ed6b138a77ca07a7cf6142a3c7d4931bf
Humberto Pinto <hlsp999@gmail.com>
2013-06-10 20:12:43 +0100

    feature

--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -1,22 +1,25 @@
-Given /^I am on (.+)$/ do |page_name|
+Given /^I am on home page$/ do
   visit root_path
 end
 
-Given /I am logged in/ do
+Given /^My name is "(.*?)"$/ do |name|
+  @user||= FactoryGirl.build(:user, name: name)
+end
+
+Given /^I am logged in/ do
   authenticate
 end
 
-Then /I should see my name/ do
-  page.should have_content(user.email)
+Then /^I should see my name/ do
+  page.should have_content(@user.name)
 end
 
-module Helpers
-  def authenticate
-    user = FactoryGirl.create(:user)
-    visit user_session
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: user.password
-    click_on "Entrar"
-  end
 
+def authenticate
+  visit new_user_session_path
+  @user = FactoryGirl.build(:user) if @user.nil?
+  @user.save!
+  fill_in :user_email, with: @user.email
+  fill_in :user_password, with: @user.password
+  click_button "Entrar"
 end
--- features/user.feature
+++ features/user.feature
@@ -4,7 +4,8 @@ Feature: Users join the site
   Should create a login or register page that works
 
   Scenario: Users can login
-    Given I am logged in
-
-
+    Given My name is "Humberto"
+    And I am on home page
+    And I am logged in
+    Then I should see my name
 
