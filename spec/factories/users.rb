66fd255e7aa659c37b8bd2a37c4e3eea50d2de14
Humberto <hlsp999@gmail.com>
2013-06-20 11:19:00 +0100

    standard passwords for users in seeds and factorygirl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryGirl.define do
   factory :user do
     name {Faker::Name.name}
     email {Faker::Internet.email}
-    password "123123"
-    password_confirmation { "123123" }
+    password "1234"
+    password_confirmation { "1234" }
     sign_in_count 1
     phone_number {Faker::PhoneNumber.phone_number}
   end
