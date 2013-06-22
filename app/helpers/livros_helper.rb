b1fb5e46a7d0061f8a79d3b82147301db3e0ea72
Humberto <hlsp999@gmail.com>
2013-06-22 01:13:58 +0100

    refactoring thumbs

--- app/helpers/livros_helper.rb
+++ app/helpers/livros_helper.rb
@@ -7,4 +7,21 @@ module LivrosHelper
     session.delete(:return_to)
   end
 
+  def main_photo_thumb(livro)
+    @book = livro
+    if photos_thumbs.nil?
+      'imagem não disponível'
+    else
+      image_tag @book.photo1.url :thumb unless @book.photo1_file_name.nil?
+    end
+  end
+
+  def photos_thumbs(livro)
+    if @book.photo1.file? || @livro.photo2.file? || @livro.photo3.file? || @livro.photo4.file?
+       image_tag @livro.photo1.url(:thumb) if @livro.photo1.file?
+       image_tag @livro.photo2.url(:thumb) if @livro.photo2.file?
+       image_tag @livro.photo3.url(:thumb) if @livro.photo3.file?
+       image_tag @livro.photo4.url(:thumb) if @livro.photo4.file?
+
+  end
 end
