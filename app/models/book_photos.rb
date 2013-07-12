b9d69adbe7910233518450e493e44770d3227e49
Humberto <hlsp999@gmail.com>
2013-07-12 19:17:42 +0100

    new model for paperclip

--- app/models/book_photos.rb
+++
@@ -1,3 +0,0 @@
-class BookPhotos < ActiveRecord::Base
-  attr_accessible :livro_id
-end
--- db/migrate/20130710192347_create_book_photos.rb
+++
@@ -1,9 +0,0 @@
-class CreateBookPhotos < ActiveRecord::Migration
-  def change
-    create_table :book_photos do |t|
-      t.integer :livro_id
-      t.attachment :photo
-      t.timestamps
-    end
-  end
-end
