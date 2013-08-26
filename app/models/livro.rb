ce5cc264f82ba2edfd8a07d2f9e2dde1580741ea
Humberto <hlsp999@gmail.com>
2013-08-27 00:19:24 +0100

    ready for jquery uploadfile with multiple files

--- app/models/livro.rb
+++ app/models/livro.rb
@@ -1,10 +1,7 @@
 class Livro < ActiveRecord::Base
-  attr_accessible :ano_escolaridade, :autore_id, :editora_id, :nome, :preco, :disciplina_id, :user_id, :photo1, :photo2, :photo3, :photo4
+  attr_accessible :ano_escolaridade, :autore_id, :editora_id, :nome, :preco, :disciplina_id, :user_id, :asset
 
-  has_attached_file :photo1, :styles => { :medium => "300x300>", :thumb => "50x100>" }, :default_url => "/images/:style/missing.png"
-  has_attached_file :photo2, :styles => { :medium => "300x300>", :thumb => "50x100>" }, :default_url => "/images/:style/missing.png"
-  has_attached_file :photo3, :styles => { :medium => "300x300>", :thumb => "50x100>" }, :default_url => "/images/:style/missing.png"
-  has_attached_file :photo4, :styles => { :medium => "300x300>", :thumb => "50x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :asset, :styles => { :medium => "300x300>", :thumb => "50x100>" }, :default_url => "/images/:style/missing.png"
 
   belongs_to :autore
   belongs_to :disciplina
